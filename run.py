73fff5684d3b89d30782f77d3d579bc3fc906651
jatoad <jasperhmca.todd@gmail.com>
2023-05-15 13:09:46 +0000

    added nav for about and home page. linked using flask

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route("/about")
+def hello_world():
+    return render_template("about.html")    
 
 if __name__ == "__main__":
     app.run(
